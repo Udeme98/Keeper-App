366a4139bb36395851bcb32a0b3f66ae7e1a38a9
Udeme98 <Darealudeme1@gmail.com>
2024-07-11 04:37:43 +0100

    Keeper app: Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "./notes";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note title={note.title} content={note.content} />
+      ))}
+
       <Footer />
     </>
   );
