ca4295ed83a69422a13caff851e19b7c65076c57
Udeme98 <Darealudeme1@gmail.com>
2024-07-11 04:18:22 +0100

    Keeper app: Notes and Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Footer from "./Footer";
 import Header from "./Header";
+import Note from "./Note";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Note />
+      <Footer />
     </>
   );
 };
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright &copy; {currentYear}</p>
+    </footer>
+  );
+};
+export default Footer;
